c9cc3cf9573f63f6afb04da1c91f61240fc51ef6
jozelot_ <jozelot77@gmail.com>
2025-11-17 15:09:43 +0100

    Update VaroX.java

--- src/main/java/de/jozelot/varoX/VaroX.java
+++ src/main/java/de/jozelot/varoX/VaroX.java
@@ -7,8 +7,21 @@ public final class VaroX extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Plugin activated!");
-        getServer().getPluginManager().getEventManager().register(new JoinLeaveListener());
+        
+        getLogger().info("Setting up Listener and Commands!");
+
+        // Listener Initialize
+        Bukkit.getPluginManager().registerEvents(new JoinLeaveListener(), this);
+
+        // Commands Initialize
+        // getCommand("test").setExecutor(new TestCommand());
+
+        Bukkit.getConsoleSender().sendMessage("§7===== §6[ VaroX ] §7=====");
+        Bukkit.getConsoleSender().sendMessage("");
+        Bukkit.getConsoleSender().sendMessage("§aPlugin version: §c" + getDescription().getVersion());
+        Bukkit.getConsoleSender().sendMessage("§aMinecraft version: §c"+ Bukkit.getBukkitVersion());
+        Bukkit.getConsoleSender().sendMessage("");
+        Bukkit.getConsoleSender().sendMessage("§7===== §6[ VaroX ] §7=====");
 
     }
 
