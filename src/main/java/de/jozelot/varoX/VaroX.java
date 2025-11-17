66478fcfae3d79453d6910cacd02adeea4ee5529
jozelot_ <jozelot77@gmail.com>
2025-11-17 13:10:53 +0100

    Init

---
+++ src/main/java/de/jozelot/varoX/VaroX.java
@@ -0,0 +1,17 @@
+package de.jozelot.varoX;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VaroX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin activated!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/de/jozelot/varoX/listeners/JoinLeaveListener.java
@@ -0,0 +1,4 @@
+package de.jozelot.varoX.listeners;
+
+public class JoinLeaveListener {
+}
