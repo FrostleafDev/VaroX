c315dfbfec156301f7fe69e41bf89bc8ef3767f0
jozelot_ <jozelot77@gmail.com>
2025-11-17 14:31:31 +0100

    Register JoinLeaveListener on plugin enable

--- src/main/java/de/jozelot/varoX/VaroX.java
+++ src/main/java/de/jozelot/varoX/VaroX.java
@@ -1,12 +1,14 @@
 package de.jozelot.varoX;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import de.jozelot.varox.listeners.JoinLeaveListener;
 
 public final class VaroX extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Plugin activated!");
+        getServer().getPluginManager().getEventManager().register(new JoinLeaveListener());
 
     }
 
